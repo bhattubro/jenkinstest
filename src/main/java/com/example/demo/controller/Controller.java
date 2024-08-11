907a5ee11a2accfc97b684e5258913cb434086e5
bhattubro <theeverything279@gmail.com>
2024-08-11 12:52:44 +0530

    3rd commit

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping("/msg")
+	public String helloWorld()
+	{
+		return "hey world!";
+	}
+	
+
+}
